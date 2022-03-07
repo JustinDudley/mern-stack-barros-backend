7fe91982be304efb2103ddfe81b649808e9a75ad
Justin Dudley <justindudley99@gmail.com>
2022-03-07 16:22:27 -0500

    added a bunch of functionality, and a .env

--- README.txt
+++ README.txt
@@ -1,12 +1,33 @@
-Following a guid from Sam Barros, 2019.
-MERN stack.
-
+Following a guide from Sam Barros, 2019.   MERN stack.
 https://medium.com/swlh/how-to-create-your-first-mern-mongodb-express-js-react-js-and-node-js-stack-7e8b20463e66
-
-
 I've named my project mern-stack-barros-backend, instead of movies-app
 
+
 To run:
-1.  cd to server folder
+1.  CD TO SERVER FOLDER !!
 2.  $ node index.js
 
+(( 3. In theory, should be able to do $ nodemon index.js, and then app listens for changes, but it's not working))
+
+
+To connect to my Atlas cluster for this project:
+(see .env for username, user-password)
+mongosh "mongodb+srv://<username>:<user-password>@mern-stack-barros-clust.0gjsv.mongodb.net/admin"
+
+
+To add a movie to our database via postman:
+http://localhost:3000/api/movie
+POST
+Body (json):
+{
+    "name": "Avengers: Endgame",
+    "time": ["14:15", "16:00", "21:30", "23:00"],
+    "rating": 8.8
+}
+
+
+---- ---- --- ----- ----- ----- ----- 
+---- ---- --- ----- ----- ----- ----- 
+
+"We need to create a connection from our server using the Mongoose library."
+See Explanations Bookmark for how to handle process.env, and the .env file
--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
+require('dotenv').config();
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+
+const db = require('./db')
+const movieRouter = require('./routes/movie-router')
+
 const app = express()
 const apiPort = 3000
 
@@ -8,8 +14,32 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 app.use(bodyParser.json())
 
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+app.use('/api', movieRouter)
+
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
+
+
+
+
+// HERE IS THE ORIGINAL RUNNING SERVER, BEFORE ADDING DB CONNECTION STUFF:
+// const express = require('express')
+// const bodyParser = require('body-parser')
+// const cors = require('cors')
+// const app = express()
+// const apiPort = 3000
+
+// app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(cors())
+// app.use(bodyParser.json())
+
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+// })
+
+// app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
